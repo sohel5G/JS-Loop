363f4d490ee55e6bd26dfad8edcf2cd4aefa87e0
sohel5G <contactsohelrana>
2023-08-01 20:23:53 +0600

    3 task done

---
+++ loop.js
@@ -0,0 +1,25 @@
+
+
+// let x = 1;
+
+// while( x <= 10 ){
+//     console.log(x)
+//     x += 2;
+// }
+
+
+// for(var i = 1; i <= 10; i += 2){
+//     console.log(i);
+// }
+
+
+// var myArr = ['Araf', 'Sohel', 'Nila', 10, 'Arif', 'Liza'];
+// for(var i = 0; i < myArr.length; i++){
+//     console.log(myArr[i]);
+// }
+
+
+let numbers = [1, 2, 3.765, 4, 5.343, 6, 7, 8, 9.64987654321];
+console.log(Math.max.apply(null, numbers));
+
+
---
+++ task.js
@@ -0,0 +1,36 @@
+/*
+// Task 1)  Display 30 time ajke amr jor jor lagtese. 
+
+let ajkeJor = 1; 
+while( ajkeJor <= 39 ){
+    console.log('Ajke amr jor jor lagtese');
+    ajkeJor++
+}
+*/
+
+
+
+/*
+// Task 2) Display number 58 - 98 
+
+for(let x = 58; x <= 98; x++){
+    console.log(x);
+}
+*/
+
+
+
+/*
+// Task 3) Display all even number between 412 - 456
+let evenNumber = 412;
+while( evenNumber < 456 ){
+    console.log(evenNumber);
+    evenNumber += 2;
+}
+*/
+
+
+
+
+
+
